cdaa85943b1833f67ae802b0258fde85756fd9e5
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2015-04-15 12:53:19 -0700

    cleanup

--- src/kvserialization.cpp
+++ src/kvserialization.cpp
@@ -25,24 +25,6 @@ void kv_encode_uint64(std::string & buffer, uint64_t value)
 
 static inline size_t internal_cstr_decode_uint64(const char * buffer, size_t size, size_t position, uint64_t * p_value)
 {
-#if 0
-    uint64_t value = 0;
-    int s = 0;
-    
-    while (position < size) {
-        unsigned char remainder = buffer[position];
-        position ++;
-        value += ((uint64_t) remainder & 0x7f) << s;
-        if ((remainder & 0x80) == 0) {
-            break;
-        }
-        s += 7;
-    }
-    
-    * p_value = value;
-    
-    return position;
-#endif
     const char * p = buffer + position;
     const char * final = p + size;
     uint64_t value = 0;
@@ -63,13 +45,6 @@ static inline size_t internal_cstr_decode_uint64(const char * buffer, size_t siz
     position = p + 1 - buffer;
     return position;
 }
-
-/*
-size_t kv_cstr_decode_uint64(const char * buffer, size_t size, size_t position, uint64_t * p_value)
-{
-    return internal_cstr_decode_uint64(buffer, size, position, p_value);
-}
- */
 
 size_t kv_decode_uint64(std::string & buffer, size_t position, uint64_t * p_value)
 {
--- src/kvserialization.h
+++ src/kvserialization.h
@@ -6,28 +6,9 @@
 
 void kv_encode_uint64(std::string & buffer, uint64_t value);
 size_t kv_decode_uint64(std::string & buffer, size_t position, uint64_t * p_value);
-//size_t kv_cstr_decode_uint64(const char * buffer, size_t size, size_t position, uint64_t * p_value);
 
 static inline size_t kv_cstr_decode_uint64(const char * buffer, size_t size, size_t position, uint64_t * p_value)
 {
-#if 0
-    uint64_t value = 0;
-    int s = 0;
-    
-    while (position < size) {
-        unsigned char remainder = buffer[position];
-        position ++;
-        value += ((uint64_t) remainder & 0x7f) << s;
-        if ((remainder & 0x80) == 0) {
-            break;
-        }
-        s += 7;
-    }
-    
-    * p_value = value;
-    
-    return position;
-#endif
     const char * p = buffer + position;
     const char * final = p + size;
     uint64_t value = 0;
